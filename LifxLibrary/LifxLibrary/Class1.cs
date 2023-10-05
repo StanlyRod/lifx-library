d240804aa03f81dfcbc83ea9cab3fe95ea10a77b
Stanly Rodriguez <stanlywgr@outlook.com>
2023-10-04 22:13:00 -0400

    first commit

---
+++ LifxLibrary/LifxLibrary/Class1.cs
@@ -0,0 +1,80 @@
+﻿using RestWrapper;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace LifxLibrary
+{
+    public class LifxBulbs
+    {
+        private string TokenKey { get; set; }
+
+        private string LightLabel { get; set; }
+
+        public LifxBulbs(string tokenKey, string lightLabel) 
+        {
+            TokenKey = tokenKey;
+            LightLabel = lightLabel;
+        }
+
+        
+        #region bulb methods
+        //synchronous method to toggle the light bulb
+        public void LightToggle()
+        {
+             RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/toggle", HttpMethod.Post);
+             req.Headers.Add("Authorization", $"Bearer {TokenKey}");
+             RestResponse resp = req.Send();
+             if (resp.StatusCode == 401)
+             {
+                throw new Exception($"HttpRequestError the token key is not valid code {resp.StatusCode}");
+             }
+             else if (resp.StatusCode == 404)
+             {
+                throw new Exception($"HttpRequestError the label name is not valid code {resp.StatusCode}");
+             }
+        }
+
+
+        public void PutBrightness(int intensity)
+        {
+            if(intensity < 0 || intensity > 100)
+            {
+                throw new ArgumentOutOfRangeException("Error the brightness level have to be set between 0 and 100");
+            }
+
+            double brightnessLevel = (double)intensity / 100.0;
+
+            //anonymous object type
+            var payload = new
+            {
+                brightness = brightnessLevel
+            };
+
+            // convert the csharp objects to json objects
+            var sharpToJson = JsonSerializer.Serialize(payload);
+
+            RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/state", HttpMethod.Put);
+            req.ContentType = "application/json";
+            req.Headers.Add("Authorization", $"Bearer {TokenKey}");
+            RestResponse resp = req.Send(sharpToJson);//send data to the api
+        }
+
+        //asynchronous method to toggle the light bulb
+        public async Task AsyncLightToggle()
+        {
+            RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/toggle", HttpMethod.Post);
+            req.Headers.Add("Authorization", $"Bearer {TokenKey}");
+            RestResponse resp = await req.SendAsync();
+            if (resp.StatusCode == 401)
+            {
+               throw new Exception($"HttpRequestError the token key is not valid code {resp.StatusCode}");
+            }
+            else if (resp.StatusCode == 404)
+            {
+               throw new Exception($"HttpRequestError the label name is not valid code {resp.StatusCode}");
+            }
+        }
+        #endregion
+    }
+}
