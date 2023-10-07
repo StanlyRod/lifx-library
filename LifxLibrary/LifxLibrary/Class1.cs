fc62444a8b3a9a12ba5648f128dce9aaed34417c
Stanly Rodriguez <stanlywgr@outlook.com>
2023-10-06 23:34:08 -0400

    multiples changes

--- LifxLibrary/LifxLibrary/Class1.cs
+++ LifxLibrary/LifxLibrary/Class1.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LifxLibrary
 {
@@ -17,6 +18,35 @@ namespace LifxLibrary
             LightLabel = lightLabel;
         }
 
+
+        private void ExceptionsThrower(RestResponse resp)
+        {
+            if (resp.StatusCode == 401)
+            {
+                throw new Exception("The token key is not valid code");
+            }
+            else if (resp.StatusCode == 404)
+            {
+                throw new Exception("The label name did not match the light");
+            }
+            else if(resp.StatusCode == 400)
+            {
+                throw new Exception("Request was invalid");
+            }
+            else if(resp.StatusCode == 500 || resp.StatusCode == 502 || resp.StatusCode == 503 || resp.StatusCode == 523)
+            {
+                throw new Exception("Server error Something went wrong on LIFX's end");
+            }
+            else if(resp.StatusCode == 422)
+            {
+                throw new Exception("Missing or malformed value");
+            }
+            else if (resp.StatusCode == 429)
+            {
+                throw new Exception("Error Too Many Requests.");
+            }
+        }
+
         
         #region toggle bulb methods
         //synchronous method to toggle the light bulb
@@ -25,33 +55,21 @@ namespace LifxLibrary
              RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/toggle", HttpMethod.Post);
              req.Headers.Add("Authorization", $"Bearer {TokenKey}");
              RestResponse resp = req.Send();
-             if (resp.StatusCode == 401)
-             {
-                throw new Exception($"HttpRequestError the token key is not valid code {resp.StatusCode}");
-             }
-             else if (resp.StatusCode == 404)
-             {
-                throw new Exception($"HttpRequestError the label name is not valid code {resp.StatusCode}");
-             }
+
+             ExceptionsThrower(resp);
         }
 
 
         
 
         //asynchronous method to toggle the light bulb
-        public async Task AsyncLightToggle()
+        public async Task LightToggleAsync()
         {
             RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/toggle", HttpMethod.Post);
             req.Headers.Add("Authorization", $"Bearer {TokenKey}");
             RestResponse resp = await req.SendAsync();
-            if (resp.StatusCode == 401)
-            {
-               throw new Exception($"HttpRequestError the token key is not valid code {resp.StatusCode}");
-            }
-            else if (resp.StatusCode == 404)
-            {
-               throw new Exception($"HttpRequestError the label name is not valid code {resp.StatusCode}");
-            }
+            
+            ExceptionsThrower(resp);
         }
         #endregion
 
@@ -80,11 +98,14 @@ namespace LifxLibrary
             req.ContentType = "application/json";
             req.Headers.Add("Authorization", $"Bearer {TokenKey}");
             RestResponse resp = req.Send(sharpToJson);//send data to the api
+
+            ExceptionsThrower(resp);
+
         }
 
 
         //Asynchronous method to change the light bulb brightness
-        public async Task AsyncPutBrightness(int intensity)
+        public async Task PutBrightnessAsync(int intensity)
         {
             if (intensity < 0 || intensity > 100)
             {
@@ -100,13 +121,61 @@ namespace LifxLibrary
             };
 
             // convert the csharp objects to json objects
-            var sharpToJson = JsonSerializer.Serialize(payload);
+            var csharpToJson = JsonSerializer.Serialize(payload);
 
             //send http request
             RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/state", HttpMethod.Put);
             req.ContentType = "application/json";
             req.Headers.Add("Authorization", $"Bearer {TokenKey}");
-            RestResponse resp = await req.SendAsync(sharpToJson);//send data to the api
+            RestResponse resp = await req.SendAsync(csharpToJson);//send data to the api
+
+            ExceptionsThrower(resp);
+
+        }
+
+
+
+        //synchronous method to change the light bulb color
+        public void PutColor(string colorValue)
+        {
+            //anonymous object type
+            var payload = new
+            {
+                color = colorValue
+            };
+
+            // convert the csharp objects to json objects
+            var csharpToJson = JsonSerializer.Serialize(payload);
+
+            //send http request
+            RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/state", HttpMethod.Put);
+            req.ContentType = "application/json";
+            req.Headers.Add("Authorization", $"Bearer {TokenKey}");
+            RestResponse resp = req.Send(csharpToJson);//send data to the api
+
+            ExceptionsThrower(resp);
+        }
+
+
+        //Asynchronous method to change the light bulb color
+        public async Task PutColorAsync(string colorValue)
+        {
+            //anonymous object type
+            var payload = new
+            {
+                color = colorValue
+            };
+
+            // convert the csharp objects to json objects
+            var csharpToJson = JsonSerializer.Serialize(payload);
+
+            //send http request
+            RestRequest req = new($"https://api.lifx.com/v1/lights/label:{LightLabel}/state", HttpMethod.Put);
+            req.ContentType = "application/json";
+            req.Headers.Add("Authorization", $"Bearer {TokenKey}");
+            RestResponse resp = await req.SendAsync(csharpToJson);//send data to the api
+
+            ExceptionsThrower(resp);
         }
     }
 }
---
+++ LifxLibrary/console_testlibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using LifxLibrary;
+
+try
+{ 
+
+LifxBulbs bed = new("", "Bedroom");
+
+    bed.PutColor("#F2F3F4");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
